d94b27b9506eb634847bb0705985b57d5ea37614
AfomiyaAweke13 <afomiyaaweke20@gmail.com>
2026-02-10 08:09:49 +0000

    Update Expertise.tsx

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
@@ -76,3 +76,4 @@ function Expertise() {
 
 
 export default Expertise;
+
